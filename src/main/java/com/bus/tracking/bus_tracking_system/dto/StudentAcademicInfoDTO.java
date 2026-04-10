30dc62eb689b787ca7ef9914ba46750fea712bb7
Pisaladiti11 <pisaladiti29@gmail.com>
2026-04-10 17:20:28 +0530

    changes in dto

--- src/main/java/com/bus/tracking/bus_tracking_system/dto/StudentAcademicInfoDTO.java
+++ src/main/java/com/bus/tracking/bus_tracking_system/dto/StudentAcademicInfoDTO.java
@@ -7,7 +7,7 @@ public class StudentAcademicInfoDTO {
     private String studentClass;
     private String division;
     private String medium;
-    private LocalDate academicYear;
+    private String academicYear;
 
     public StudentAcademicInfoDTO() {
     }
@@ -15,7 +15,7 @@ public class StudentAcademicInfoDTO {
     public StudentAcademicInfoDTO(String studentClass,
                                   String division,
                                   String medium,
-                                  LocalDate academicYear) {
+                                  String academicYear) {
         this.studentClass = studentClass;
         this.division = division;
         this.medium = medium;
@@ -25,5 +25,5 @@ public class StudentAcademicInfoDTO {
     public String getStudentClass() { return studentClass; }
     public String getDivision() { return division; }
     public String getMedium() { return medium; }
-    public LocalDate getAcademicYear() { return academicYear; }
+    public String getAcademicYear() { return academicYear; }
 }
