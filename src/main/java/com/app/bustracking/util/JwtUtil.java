0de85c805e56b59ccf53ef0a2a546132cf77925b
pjsofttech <pjsofttech@pjsofttech.com>
2026-09-08 21:34:22 +0530

    jwt encoding format changed to UTF8

--- src/main/java/com/app/bustracking/util/JwtUtil.java
+++ src/main/java/com/app/bustracking/util/JwtUtil.java
@@ -2,13 +2,13 @@ package com.app.bustracking.util;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -24,7 +24,7 @@ public class JwtUtil {
     private Long expirationTime;
 
     private SecretKey getSigningKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        byte[] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
